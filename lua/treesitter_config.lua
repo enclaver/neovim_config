97d63fecd553f6e3ddbc0696748cd484ef81347f
Jovan Nisic <nisicjovan@hotmail.com>
2025-04-01 13:18:00 +0200

    new keybinds for next prev hunk and class

--- lua/treesitter_config.lua
+++ lua/treesitter_config.lua
@@ -71,19 +71,19 @@ vim.defer_fn(function()
         set_jumps = true, -- whether to set jumps in the jumplist
         goto_next_start = {
           [']f'] = '@function.outer',
-          [']]'] = '@class.outer',
+          [']c'] = '@class.outer',
         },
         goto_next_end = {
           [']F'] = '@function.outer',
-          [']['] = '@class.outer',
+          [']C'] = '@class.outer',
         },
         goto_previous_start = {
           ['[f'] = '@function.outer',
-          ['[['] = '@class.outer',
+          ['[c'] = '@class.outer',
         },
         goto_previous_end = {
           ['[F'] = '@function.outer',
-          ['[]'] = '@class.outer',
+          ['[C'] = '@class.outer',
         },
       },
       swap = {
